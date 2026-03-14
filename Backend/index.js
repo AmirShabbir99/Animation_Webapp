192bf0a0e43916dc9a17a8348cbe6772a20c033f
M.Amir Shabbir <amir.shabbir@eezahtech.com>
2026-03-14 23:32:38 +0500

    add env config

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,16 @@
 import express from "express"
 import mongoose from "mongoose"
+import dotenv from "dotenv"
 import Userouter from "./routes/user.routes.js"
 import cors from "cors"
 const app = express()
-const MONGOURI = "mongodb://127.0.0.1:27017/newEra"
+dotenv.config();
 app.use(express.json())
 app.use(cors())
 app.use("/",Userouter)
 console.log("Route visit done.......")
 // Corrected connection method and promise handling
-mongoose.connect(MONGOURI)
+mongoose.connect(process.env.MONGOURI)
   .then(() => console.log("DB connected"))
   .catch((err) => console.log("DB connection error:", err));
 
